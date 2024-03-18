d93eac862e889ab83b583387c6b56b5fc7cab501
andrej-djordjevic <andrejdjordjevic2003@gmail.com>
2024-03-18 17:45:19 +0100

    v2

---
+++ src/store-v2.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import accountreducer from "./features/account/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const rootReducer = combineReducers({
+  account: accountreducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
--- src/store.js
+++ src/store.js
@@ -1,14 +1,9 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountreducer from "./features/account/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  account: accountreducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: { account: accountreducer, customer: customerReducer },
 });
-
-const store = createStore(rootReducer,composeWithDevTools (applyMiddleware(thunk)));
 
 export default store;
